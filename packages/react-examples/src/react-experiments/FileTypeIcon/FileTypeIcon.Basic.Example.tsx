311ceeecb796d69c5d1e9f304638d366699f5d0a
Carlos Perez (M365) <caperez@microsoft.com>
2024-12-19 09:29:23 -0800

    yarn change

--- packages/react-examples/src/react-experiments/FileTypeIcon/FileTypeIcon.Basic.Example.tsx
+++ packages/react-examples/src/react-experiments/FileTypeIcon/FileTypeIcon.Basic.Example.tsx
@@ -9,33 +9,33 @@ export class FileTypeIconBasicExample extends React.Component<{}, {}> {
     return (
       <div>
         <h3>Size 16 accdb icon as .png</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'loop', size: 16, imageFileType: 'png' })} />
+        <Icon {...getFileTypeIconProps({ extension: 'mdb', size: 16, imageFileType: 'png' })} />
         <h3>Size 20 archive icon as .png</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'fluid', size: 20, imageFileType: 'png' })} />
+        <Icon {...getFileTypeIconProps({ extension: '7z', size: 20, imageFileType: 'png' })} />
         <h3>Size 32 audio icon as .png</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'loop', size: 32, imageFileType: 'png' })} />
+        <Icon {...getFileTypeIconProps({ extension: 'oga', size: 32, imageFileType: 'png' })} />
         <h3>Size 40 code icon as .png</h3>
-        <Icon {...getFileTypeIconProps({ extension: '.fluid', size: 40, imageFileType: 'png' })} />
+        <Icon {...getFileTypeIconProps({ extension: '.cpp', size: 40, imageFileType: 'png' })} />
         <h3>Size 48 csv icon as .png</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'loop', size: 48, imageFileType: 'png' })} />
+        <Icon {...getFileTypeIconProps({ extension: 'csv', size: 48, imageFileType: 'png' })} />
         <h3>Size 64 model icon as .png</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'fluid', size: 64, imageFileType: 'png' })} />
+        <Icon {...getFileTypeIconProps({ extension: 'blend', size: 64, imageFileType: 'png' })} />
         <h3>Size 96 docx icon as .png</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'loop', size: 96, imageFileType: 'png' })} />
+        <Icon {...getFileTypeIconProps({ extension: 'docx', size: 96, imageFileType: 'png' })} />
         <h3>Size 16 dotx icon as .svg</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'fluid', size: 16, imageFileType: 'svg' })} />
+        <Icon {...getFileTypeIconProps({ extension: 'dot', size: 16, imageFileType: 'svg' })} />
         <h3>Size 20 email icon as .svg</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'loop', size: 20, imageFileType: 'svg' })} />
+        <Icon {...getFileTypeIconProps({ extension: 'msg', size: 20, imageFileType: 'svg' })} />
         <h3>Size 32 exe icon as .svg</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'fluid', size: 32 })} />
+        <Icon {...getFileTypeIconProps({ extension: 'msi', size: 32 })} />
         <h3>Size 40 script icon as .svg</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'loop', size: 40 })} />
+        <Icon {...getFileTypeIconProps({ extension: 'osts', size: 40 })} />
         <h3>Size 48 html icon as .svg</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'fluid', size: 48 })} />
+        <Icon {...getFileTypeIconProps({ extension: 'html', size: 48 })} />
         <h3>Size 64 mpp icon as .svg</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'loop', size: 64 })} />
+        <Icon {...getFileTypeIconProps({ extension: 'mpp', size: 64 })} />
         <h3>Size 96 link icon as .svg</h3>
-        <Icon {...getFileTypeIconProps({ extension: 'fluid', size: 96 })} />
+        <Icon {...getFileTypeIconProps({ extension: 'url', size: 96 })} />
         <h3>Size 16 docset icon as .png</h3>
         <Icon {...getFileTypeIconProps({ type: FileIconType.docset, size: 16, imageFileType: 'png' })} />
         <h3>Size 20 folder icon as .svg</h3>
